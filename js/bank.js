66b583a74b6d5bfe1755f15fc0f2bfeda803ef5b
mdtalhaba <jubairt35@gmail.com>
2022-08-19 21:03:54 +0600

    first commit

---
+++ js/bank.js
@@ -0,0 +1,63 @@
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+    depositField.value = '';
+
+    if (isNaN(newDepositAmount)) {
+        alert('Please provide a valid number');
+        return;
+    }
+
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+})
+
+
+
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    withdrawField.value = '';
+
+    if (isNaN(newWithdrawAmount)) {
+        alert('Please provide a valid number');
+        return;
+    }
+
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Your current balance is low');
+        return;
+    }
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+
+
+})
